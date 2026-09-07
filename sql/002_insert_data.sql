04926eb1b441853547755cca3bbdca4fcc13b386
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2026-09-07 21:23:48 +0530

    Update 002_insert_data.sql

--- sql/002_insert_data.sql
+++ sql/002_insert_data.sql
@@ -4,3 +4,5 @@ VALUES
 (1, 'Rahul', 'rahul@example.com', 'Tanuku', CURRENT_TIMESTAMP()),
 (2, 'John', 'john@example.com', 'Hyderabad', CURRENT_TIMESTAMP()),
 (3, 'Priya', 'priya@example.com', 'Vijayawada', CURRENT_TIMESTAMP());
+
+
