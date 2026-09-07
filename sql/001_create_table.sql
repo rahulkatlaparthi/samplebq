872a608aa666aea56a46e97bc213c239ed45db76
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2026-09-07 21:06:43 +0530

    Create 001_create_table.sql

---
+++ sql/001_create_table.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS `qwiklabs-gcp-00-38c5c3a6722c.demo_dataset`;
+
+CREATE TABLE IF NOT EXISTS `qwiklabs-gcp-00-38c5c3a6722c.demo_dataset.customers`
+(
+  customer_id INT64,
+  customer_name STRING,
+  email STRING,
+  city STRING,
+  created_at TIMESTAMP
+);
